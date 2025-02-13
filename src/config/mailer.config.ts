6e933e17e37dc07998de84897c985f4ee7d9ba7f
VS <vvs.jkr@gmail.com>
2025-02-13 15:38:55 +0300

    fix bug

--- src/config/mailer.config.ts
+++ src/config/mailer.config.ts
@@ -1,21 +1,20 @@
 import { MailerOptions } from '@nestjs-modules/mailer'
 import { ConfigService } from '@nestjs/config'
 
-
 export const getMailerConfig = async (
 	configService: ConfigService
- ): Promise<MailerOptions> => ({
+): Promise<MailerOptions> => ({
 	transport: {
 		host: configService.get<string>('MAILER_HOST'),
 		port: configService.get<number>('MAILER_PORT'),
 		secure: false,
 		requireTLS: true,
-	  auth: {
-		user: configService.get<string>('MAILER_USER'),
-		pass: configService.get<string>('MAILER_PASS'),
-	},
+		auth: {
+			user: configService.get<string>('MAILER_USER'),
+			pass: configService.get<string>('MAILER_PASS')
+		}
 	},
 	defaults: {
-	  from: '<MS_xFUvWZ@trial-zr6ke4n3knylon12.mlsender.net>', 
-	},
- });
+		from: configService.get<string>('MAILER_FROM_EMAIL')
+	}
+})
